7fabc9d7131caace399bb75f6fc57375fda2ad82
ramkrishna <ramkrishna.kc@edetek.com>
2022-05-31 23:09:22 +0545

    Missing server-side file

---
+++ server/models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 5,
+    maxlength: 12,
+    index: true,
+  },
+  password: { type: String, required: true },
+  role: { type: String, default: "user", enum: ["admin", "user"] },
+  verified: { type: Boolean, default: false },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: [
+      email => (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/).test(email),
+      'Please fill a valid email address'
+    ],
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      'Please fill a valid email address'
+    ],
+  },
+  name: { type: String },
+  address: { type: String },
+  contactNum: { type: String, unique: true },
+}, { timestamps : true });
+
+module.exports = mongoose.model("users", userSchema);
